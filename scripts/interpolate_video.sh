99f57aa358e2ca4c1cc786c0910ea5063660df10
Stefan Sauer <stefan.sauer01@googlemail.com>
2020-12-14 18:20:57 +0100

    added simple bash/batch script for interpolating video

---
+++ scripts/interpolate_video.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python AdaCoF\interpolate_video.py --input_video ./Testset/Clip1 --output_video tmp/out/ --checkpoint AdaCoF/checkpoint/kernelsize_5/ckpt.pth --config AdaCoF//checkpoint/kernelsize_5/config.txt
