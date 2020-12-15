a14674d045b9b9743873dc1ae72e9e2e7a4a607a
Stefan Sauer <stefan.sauer01@googlemail.com>
2020-12-15 13:57:45 +0100

    continued evaluation script

--- Evaluation/evaluate.py
+++ Evaluation/evaluate.py
@@ -23,38 +23,65 @@ def evaluate_image(prediction, target):
 # and saves the result at output
 def interpolate(a, b, output):
     print(f'Interpolating {a} and {b} to {output}')
-    subprocess.run(
-        f'{sys.executable} ' \
-        '../AdaCoF/interpolate_twoframe.py ' \
-        '--first_frame {a} ' \
-        '--second_frame {b} ' \
-        '--output_frame {output} ' \
-        '--checkpoint ../AdaCoF/checkpoint/kernelsize_5/ckpt.pth ' \
-        '--config ../AdaCoF/checkpoint/kernelsize_5/config.txt')
+    subprocess.run([
+        f'{sys.executable}',
+        '../AdaCoF/interpolate_twoframe.py',
+        f'--first_frame', a,
+        f'--second_frame', b,
+        f'--output_frame', output,
+        '--checkpoint', '../AdaCoF/checkpoint/kernelsize_5/ckpt.pth',
+        '--config', '../AdaCoF/checkpoint/kernelsize_5/config.txt'])
 
 # Interpolates triples of images
 # from a dataset (list of filenames)
 def interpolate_dataset(dataset_path):
-    print(f'Interpolating Dataset {dataset_path}')
-    dataset = glob.glob(f'{dataset_path}/*.png')
+    dataset_name = os.path.basename(dataset_path)
+    print(f'Interpolating Dataset {dataset_name}')
+    dataset = sorted(glob.glob(f'{dataset_path}/*.png'))
     it = range(0, len(dataset)-2)
     print(dataset_path)
     for i in tqdm(iterable=it, total=len(it)):
-        interpolated_filename = os.path.splitext(dataset[i+1])[1]
-        interpolate(dataset[i], dataset[i+2], f'{tmp_dir}/{interpolated_filename}')
+        interpolated_filename = os.path.basename(dataset[i+1])
+        output_path = f'{tmp_dir}/{dataset_name}/{interpolated_filename}'
+        os.makedirs(f'{tmp_dir}/{dataset_name}', exist_ok=True)
+        interpolate(dataset[i], dataset[i+2], output_path)
 
 # Evaluates a dataset.
 # Takes interpolated images a and c
 # and compares the result with b
-def evaluate_dataset(dataset_path):
-    prediction = datasets.ImageFolder(tmp_dir)
-    target = datasets.ImageFolder(dataset_path)
-    return evaluate_image(prediction, target)
+def evaluate_datasets(dataset_path):
+    predictionFolder = datasets.ImageFolder(tmp_dir, transform=transforms.ToTensor)
+    targetFolder = datasets.ImageFolder(dataset_path, transform=transforms.ToTensor)
+
+    predictionsLoader = torch.utils.data.DataLoader(predictionFolder)
+    targetsLoader = torch.utils.data.DataLoader(targetFolder)
+
+    print(len(predictionsLoader))
+    print(len(targetsLoader))
+
+    predictions = iter(predictionsLoader)
+    targets = iter(targetsLoader)
+
+    # Skip first image
+    next(targets)
+
+    results = []
+
+    #it = range(1, len(targets))
+    it = zip(predictions, targets)
+
+    for (prediction, target) in tqdm(iterable=it, total=len(predictionsLoader)):
+        result = evaluate_image(prediction, target)
+        results.append(result)
+
+    return results
 
 
 testsets = ['Clip1', 'Clip2']
 for testset in testsets:
-    testset_path = f'../Testset/{testset}'
-    interpolate_dataset(testset_path)
-    result = evaluate_dataset(testset_path)
-    print(f'Result: {result}')
+    if(not os.path.isdir(f'{tmp_dir}/{testset}')):
+        testset_path = f'../Testset/{testset}'
+        interpolate_dataset(testset_path)
+
+result = evaluate_datasets('../Testset/')
+print(f'Result: {result}')
