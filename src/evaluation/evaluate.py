changed normalization; removed unnecessary evaluation data; fixed evaluation

src/evaluation/evaluate.py
@@ -37,7 +37,7 @@ def interpolate_adacof(a, b, output):
     print('Interpolating {} and {} to {} with adacof'.format(a, b, output))
     with torch.no_grad():
         adacof_interp.interp(SimpleNamespace(
-            gpu_id=1,
+            gpu_id=0,
             model='src.adacof.models.adacofnet',
             kernel_size=5,
             dilation=1,
@@ -54,7 +54,7 @@ def interpolate_phasenet(a, b, output):
     print('Interpolating {} and {} to {} with phasenet'.format(a, b, output))
     with torch.no_grad():
         phasenet_interp.interp(SimpleNamespace(
-            gpu_id=1,
+            gpu_id=0,
             first_frame=a,
             second_frame=b,
             output_frame=output,
@@ -66,7 +66,7 @@ def interpolate_fusion(a, b, output):
     print('Interpolating {} and {} to {} with fusion method'.format(a, b, output))
     with torch.no_grad():
         fusion_interp.interp(SimpleNamespace(
-            gpu_id=1,
+            gpu_id=0,
             adacof_model='src.fusion_net.fusion_adacofnet',
             adacof_kernel_size=5,
             adacof_dilation=1,
@@ -158,7 +158,7 @@ def create_images(testset, test_path, inter_path):
         inter_image_adacof = [inter_path + i + "/adacof/" + interpolate for interpolate in sorted(os.listdir(inter_path + "/" + i + "/adacof"))]
         inter_image_phasenet = [inter_path + i + "/phasenet/" + interpolate for interpolate in sorted(os.listdir(inter_path + "/" + i + "/phasenet"))]
         inter_image_fusion = [inter_path + i + "/fusion/" + interpolate for interpolate in sorted(os.listdir(inter_path + "/" + i + "/fusion"))]
-        error = np.load("Evaluation/result_" + i + ".npy")[:, 0, :]
+        error = np.load("Evaluation/result_" + i + ".npy")
         for image_idx in range(len(inter_image_adacof) - 1): # TODO: Could be that error is missing one entry?
             draw_difference(np.asarray(Image.open(inter_image_adacof[image_idx])),
                             np.asarray(Image.open(inter_image_phasenet[image_idx])),
@@ -173,7 +173,7 @@ def create_images(testset, test_path, inter_path):
 
 
 def draw_difference(pred_img_adacof, pred_img_phasenet, pred_img_fusion, target_img, out_path, error, number):
-    name = 'img_{}_{}.png'.format(str(number).zfill(3), error[0])
+    name = 'img_{}_{}.png'.format(str(number).zfill(3), error[0, 0])
 
     if os.path.exists(out_path + "/" + name):
         return
@@ -206,19 +206,23 @@ def draw_difference(pred_img_adacof, pred_img_phasenet, pred_img_fusion, target_
     plt.imshow(difference_adacof, interpolation='none', cmap='plasma', vmin=0, vmax=255)
     plt.axis('off')
     plt.colorbar()
-    plt.title('AdaCoF Difference')
+    plt.title('AdaCoF Diff')
+    plt.text(500, 1300, 'SSIM: {:.2f}\nLPIPS: {:.2f}\nPSNR: {:.2f}'.format(error[0, 0], error[0, 1], error[0, 2]), ha='center')
 
     plt.subplot(3, 3, 8)
     plt.imshow(difference_fusion, interpolation='none', cmap='plasma', vmin=0, vmax=255)
     plt.axis('off')
     plt.colorbar()
-    plt.title('Fusion Difference')
+    plt.title('Fusion Diff')
+    plt.text(500, 1300, 'SSIM: {:.2f}\nLPIPS: {:.2f}\nPSNR: {:.2f}'.format(error[1, 0], error[1, 1], error[1, 2]), ha='center')
+
 
     plt.subplot(3, 3, 9)
     plt.imshow(difference_phasenet, interpolation='none', cmap='plasma', vmin=0, vmax=255)
     plt.axis('off')
     plt.colorbar()
-    plt.title('Phasenet Difference')
+    plt.title('Phasenet Diff')
+    plt.text(500, 1300, 'SSIM: {:.2f}\nLPIPS: {:.2f}\nPSNR: {:.2f}'.format(error[2, 0], error[2, 1], error[2, 2]), ha='center')
 
     plt.savefig(out_path + "/" + name, dpi=600)
     plt.clf()
@@ -267,6 +271,8 @@ def draw_measurements(datasets, datasets_results, title):
 
     legend_order = [1,2,0]
 
+    plt.figure(figsize=(2, 3))
+
     plt.suptitle(title)
     
     plt.subplot(1, 3, 1)
@@ -306,8 +312,12 @@ def draw_measurements(datasets, datasets_results, title):
 #testsets = ['Clip1', 'Clip2', 'Clip3', 'Clip4', 'Clip5', 'Clip6', 'Clip7', 'Clip8', 'Clip9', 'Clip10', 'Clip11', \
 #    'airboard_1', 'airplane_landing', 'airtable_3', 'basketball_1', 'water_ski_2', 'yoyo']
 
+#testsets = ['Clip{}'.format(i) for i in range(1, 7)]
+
+testsets = ['MODE_SH0280', 'MODE_SH0440', 'MODE_SH0450', 'MODE_SH0740', 'MODE_SH0780', 'MODE_SH1010', 'MODE_SH1270']
+
 #testsets = ['Clip1']
-testsets = ['MODE_SH1010']
+#testsets = ['MODE_SH1010']
 
 # Interpolate
 for testset in testsets:

src/fusion_net/interpolate_twoframe.py
@@ -67,7 +67,7 @@ def interp(args):
     shape_r = img_1.shape
 
     with torch.no_grad():
-        frame_out1, frame_out2 = adacof_model(
+        frame_out1, frame_out2, _ = adacof_model(
             torch.as_tensor(img_1).permute(2, 0, 1).float().unsqueeze(0).to(device)/255,
             torch.as_tensor(img_2).permute(2, 0, 1).float().unsqueeze(0).to(device)/255)
         frame_out1, frame_out2 = frame_out1.squeeze(0).permute(1, 2, 0).cpu().numpy(), frame_out2.squeeze(0).permute(1, 2, 0).cpu().numpy()
@@ -79,14 +79,10 @@ def interp(args):
     frame_out2_pad = pad_img(frame_out2)
 
     # To tensors
-    img_1 = rgb2lab(torch.as_tensor(img_1_pad).permute(2, 0, 1).float()).to(device)
-    img_2 = rgb2lab(torch.as_tensor(img_2_pad).permute(2, 0, 1).float()).to(device)
+    img_1 = rgb2lab_single(torch.as_tensor(img_1_pad).permute(2, 0, 1).float()).to(device)
+    img_2 = rgb2lab_single(torch.as_tensor(img_2_pad).permute(2, 0, 1).float()).to(device)
     frame_1 = torch.as_tensor(frame_out1_pad).permute(2, 0, 1).float().to(device)
     frame_2 = torch.as_tensor(frame_out2_pad).permute(2, 0, 1).float().to(device)
-
-    # Show frame
-    transforms.ToPILImage()(img_1).show()
-    transforms.ToPILImage()(frame_1).show()
 
     # Build pyramid
     pyr = Pyramid(
@@ -130,7 +126,7 @@ def interp(args):
 
     # Put picture together
     result = torch.cat(result, 0)
-    img_p = lab2rgb(result)
+    img_p = lab2rgb_single(result)
 
     img_p = img_p[:, :shape_r[0], :shape_r[1]]
 

src/phase_net/interpolate_twoframe.py
@@ -44,8 +44,8 @@ def interp(args):
     img_2 = pad_img(img_2/255)
 
     # To tensors
-    img_1 = rgb2lab(torch.as_tensor(img_1).permute(2, 0, 1).float()).to(device)
-    img_2 = rgb2lab(torch.as_tensor(img_2).permute(2, 0, 1).float()).to(device)
+    img_1 = rgb2lab_single(torch.as_tensor(img_1).permute(2, 0, 1).float()).to(device)
+    img_2 = rgb2lab_single(torch.as_tensor(img_2).permute(2, 0, 1).float()).to(device)
 
 
     # Build pyramid
@@ -86,13 +86,9 @@ def interp(args):
 
     # Put picture together
     result = torch.cat(result, 0)
-    img_p = lab2rgb(result)
+    img_p = lab2rgb_single(result)
 
-    print(img_p.shape)
     img_p = img_p[:, :shape_r[0], :shape_r[1]]
-    print(img_p.shape)
-    # Show frame
-    #transforms.ToPILImage()(img_p).show()
 
     imwrite(img_p.clone(), args.output_frame, range=(0, 1))
 

src/train/transform.py
@@ -7,9 +7,19 @@ def rgb2lab(img: torch.Tensor):
     """ Transforms a batch of rgb images into lab color space. Dimensions: [B, C, H, W] """
     lab = color.rgb2lab(img.cpu().detach().permute(0, 2, 3, 1).numpy())
     lab[:,:,:,0] /= 100
-    lab[:,:,:,1:] += 128
-    lab[:,:,:,1:] /= 255
+    #lab[:,:,:,1:] += 128
+    lab[:,:,:,1:] /= 128#255
     lab = torch.tensor(lab).permute(0, 3, 1, 2)
+
+    return lab
+    
+def rgb2lab_single(img: torch.Tensor):
+    """ Transforms a batch of rgb images into lab color space. Dimensions: [C, H, W] """
+    lab = color.rgb2lab(img.cpu().detach().permute(1, 2, 0).numpy())
+    lab[:,:,0] /= 100
+    #lab[:,:,1:] += 128
+    lab[:,:,1:] /= 128#255
+    lab = torch.tensor(lab).permute(2, 0, 1)
 
     return lab
 
@@ -17,8 +27,8 @@ def lab2rgb(img: torch.Tensor):
     """ Transforms a batch of lab images into rgb color space. Dimensions: [B, C, H, W] """
     rgb = img.clone().cpu().detach().permute(0, 2, 3, 1).numpy()
     rgb[:,:,:,0] *= 100
-    rgb[:,:,:,1:] *= 255
-    rgb[:,:,:,1:] -= 128
+    rgb[:,:,:,1:] *= 128#255
+    #rgb[:,:,:,1:] -= 128
     rgb = color.lab2rgb(rgb)
     rgb = torch.tensor(rgb).permute(0, 3, 1, 2)
 
@@ -28,8 +38,8 @@ def lab2rgb_single(img: torch.Tensor):
     """ Transforms a single lab image into rgb color space. Dimensions: [C, H, W] """
     rgb = img.clone().cpu().detach().permute(1, 2, 0).numpy()
     rgb[:,:,0] *= 100
-    rgb[:,:,1:] *= 255
-    rgb[:,:,1:] -= 128
+    rgb[:,:,1:] *= 128#255
+    #rgb[:,:,1:] -= 128
     rgb = color.lab2rgb(rgb)
     rgb = torch.tensor(rgb).permute(2, 0, 1)
 